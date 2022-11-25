7ff12ac47facfcebf796d98f8d42e998f2fb54f5
Pablo Carrizo <pablocarrizo331@gmail.com>
2022-11-25 18:39:16 -0300

    Actualizacion de html arch static

--- maverick/appMaverick/urls.py
+++ maverick/appMaverick/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import menu
+from .views import menu, leftside, noside, rightside
 
 urlpatterns = [
-    path('menu/',menu),
+    path('',menu, name='menu'),
+    path('izquierda/',leftside, name='izquierda'),
+    path('barra/',noside, name='barra'),
+    path('derecha/',rightside, name='derecha'),
 ]
--- maverick/appMaverick/views.py
+++ maverick/appMaverick/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def menu(request):
-    return render(request,'appMaverick/menu.html')
+    return render(request,'appMaverick/index.html')
+def leftside(request):
+    return render(request,'appMaverick/left-sidebar.html')
+def noside(request):
+    return render(request,'appMaverick/no-sidebar.html')
+def rightside(request):
+    return render(request,'appMaverick/right-sidebar.html')            
