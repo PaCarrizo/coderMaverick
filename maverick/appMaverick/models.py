9e7f637d052a2b71c1d7b426f4e3ad2614f4600f
Jose Cruz <jose.l.cruz77@gmail.com>
2022-11-29 20:06:55 -0300

    creacion de modelos y formularios

--- maverick/appMaverick/models.py
+++ maverick/appMaverick/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Recomendacion(models.Model):
+    nombre = models.CharField(max_length=40)
+    correo = models.EmailField()
+    comida = models.CharField(max_length=11)
+    comentario = models.CharField(max_length=500)
+
+class Post(models.Model):
+    nombre = models.CharField(max_length=50)
+    articulo = models.CharField(max_length=500)
+    imagen = models.URLField()
+    autor = models.CharField(max_length=50)
+
+class Reclamos(models.Model):
+    nombre = models.CharField(max_length=40)
+    email = models.EmailField()
+    categoria = models.CharField(max_length=11)
+    nota = models.CharField(max_length=500)
+    
